432d03dd3e442cd64a8cc1481ec7586fb56fe5d8
miskat433 <miskat433@gmail.com>
2022-11-08 18:21:18 +0600

    comment added route add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const PORT = process.env.PORT || 5000;
 
 
@@ -19,9 +19,47 @@ app.get("/", (req, res) => {
 
 async function run() {
     try {
+        const servicesCollection = client.db("perfectClick").collection("services");
+        const reviewCollection = client.db("perfectClick").collection("review");
         const albumCollection = client.db("perfectClick").collection("album");
         const workInfoCollection = client.db("perfectClick").collection("workInfo");
 
+        // service start
+
+        app.get("/services", async (req, res) => {
+            const query = {}
+            if (req.query.limit) {
+                const cursor = servicesCollection.find(query).limit(parseInt(req.query.limit))
+                const result = await cursor.toArray()
+                return res.send(result)
+            }
+            const cursor = servicesCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get("/singleService/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const cursor = servicesCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // service end
+
+        // comments start
+
+        app.post("/addComment", async (req, res) => {
+            const data = req.body
+            const cursor = await reviewCollection.insertOne(data)
+            res.send(cursor)
+        })
+        // comments end
+
+
+
+        // Other start
 
         app.get("/workInfo", async (req, res) => {
             const query = {}
@@ -36,6 +74,8 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result)
         })
+
+        // Other end
 
     }
     finally { }
